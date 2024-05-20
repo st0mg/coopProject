cb059b3a77b26cdaf89343a29c6fa77daf0ecf97
StOMG <godemperor228666@gmail.com>
2024-05-20 18:28:51 +0300

    section done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 window.addEventListener("scroll", function(event){
     hideHeader(event)
+    show()
 })
+
+show()
 
 let old = 0
 
 const header = document.querySelector("header")
 
-function hideHeader(event){
+function hideHeader(){
     current = window.scrollY
     down = current > old
     old = current
-    console.log(current, old);
+    // console.log(current, old);
     if (down)
     {
         if (!header.classList.contains("hideHeader"))
@@ -22,4 +25,19 @@ function hideHeader(event){
     {
         header.classList.remove("hideHeader")
     }
+}
+
+
+function show(){
+    let width = document.documentElement.clientWidth
+    let height = document.documentElement.clientHeight
+
+    let currentElem = document.elementFromPoint(width/2,height/4*3).closest("section")
+
+    if (!currentElem.classList.contains("isShow"))
+    {
+        currentElem.classList.add("isShow")
+    }
+    
+    console.log(currentElem);
 }
